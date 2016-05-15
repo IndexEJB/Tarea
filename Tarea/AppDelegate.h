35324a9de19eb5e7e13ff0786f38c962ea0b4661
Edgar Juárez Bustos <ega1_@hotmail.com>
2016-05-15 12:02:08 -0500

    Initial Commit

---
+++ Tarea/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Tarea
+//
+//  Created by MKT on 15/05/16.
+//  Copyright © 2016 MKT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Tarea/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Tarea
+//
+//  Created by MKT on 15/05/16.
+//  Copyright © 2016 MKT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
